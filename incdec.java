04b99830c0f7bfdde64737010b110a7f12875d2c
Harsimran Thethi <harsimran.thethi@gmail.com>
2018-04-01 11:08:48 +0530

    inc dec

---
+++ incdec.java
@@ -0,0 +1,10 @@
+
+public class incdec{
+  public static void main(String[] args){
+    int m = 2, n = 15;
+    for(int i=1;i<5;i++){
+      System.out.println("m = " + m++);
+      System.out.println("n = " + --n);
+    }
+  }
+}
