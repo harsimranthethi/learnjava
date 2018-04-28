aa7bad75886233ab802b81f606d039d18c875754
Harsimran Thethi <harsimran.thethi@gmail.com>
2018-04-28 12:49:03 +0530

    interactive triangle area

---
+++ itriangle.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class itriangle{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    double a,b,c;
+    System.out.println("enter first side of triangle");
+    a = sc.nextDouble();
+
+    System.out.println("enter second side of triangle");
+    b = sc.nextDouble();
+
+    System.out.println("enter third side of triangle");
+    c = sc.nextDouble();
+
+    double s = (a+b+c)/2;
+    double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+
+    if(Double.isNaN(area)) {
+      System.out.println( "i dont think this is a triangle");
+
+    } else {
+      System.out.println( "i am sure "+ area + " is the area of triangle ");
+
+    }
+
+
+  }
+}
